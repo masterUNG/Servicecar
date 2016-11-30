e93879a4cc9750c3b62b204f85cc67923d5e0a89
masterUNG <phrombutr@gmail.com>
2016-11-30 22:24:14 +0700

    Search View near OK

---
+++ app/src/main/java/nrru/chumsila/tippawan/servicecar/AnimalNames.java
@@ -0,0 +1,14 @@
+package nrru.chumsila.tippawan.servicecar;
+
+public class AnimalNames {
+    private String animalName;
+
+    public AnimalNames(String animalName) {
+        this.animalName = animalName;
+    }
+
+    public String getAnimalName() {
+        return this.animalName;
+    }
+
+}
--- app/src/main/java/nrru/chumsila/tippawan/servicecar/SearchShop.java
+++ app/src/main/java/nrru/chumsila/tippawan/servicecar/SearchShop.java
@@ -1,13 +1,62 @@
 package nrru.chumsila.tippawan.servicecar;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+import android.widget.SearchView;
 
-public class SearchShop extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class SearchShop extends AppCompatActivity implements SearchView.OnQueryTextListener {
+
+    // Declare Variables
+    ListView list;
+    ListViewAdapter adapter;
+    SearchView editsearch;
+    String[] animalNameList;
+    ArrayList<AnimalNames> arraylist = new ArrayList<AnimalNames>();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_shop);
+
+        // Generate sample data
+
+        animalNameList = new String[]{"Lion", "Tiger", "Dog",
+                "Cat", "Tortoise", "Rat", "Elephant", "Fox",
+                "Cow","Donkey","Monkey"};
+
+        // Locate the ListView in listview_main.xml
+        list = (ListView) findViewById(R.id.listview);
+
+        for (int i = 0; i < animalNameList.length; i++) {
+            AnimalNames animalNames = new AnimalNames(animalNameList[i]);
+            // Binds all strings into an array
+            arraylist.add(animalNames);
+        }
+
+        // Pass results to ListViewAdapter Class
+        adapter = new ListViewAdapter(this, arraylist);
+
+        // Binds the Adapter to the ListView
+        list.setAdapter(adapter);
+
+        // Locate the EditText in listview_main.xml
+        editsearch = (SearchView) findViewById(R.id.search);
+        editsearch.setOnQueryTextListener(this);
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        String text = newText;
+        adapter.filter(text);
+        return false;
     }
 }
